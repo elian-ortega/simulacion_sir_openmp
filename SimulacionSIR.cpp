5bd72f84f57c24e359afeeb87ccd5f7d72471a06
Ronn2227 <ronn2227@hotmail.com.com>
2020-07-14 23:00:22 -0600

    agrega pedir valores por parametro

--- SimulacionSIR.cpp
+++ SimulacionSIR.cpp
@@ -12,28 +12,42 @@
 #include "Persona.h"
 
 using namespace std;
-int main()
-{
+int main(int argc, char* argv[]){
+    //params
+	int cpr = atoi(argv[1]);    // cantidad de personas int ]0,10000000]
+	double piv = atof(argv[2]); // potencia infecciosa ]0,1[
+	double prj = atof(argv[3]); // probabilidad de recuperacion jovenes ]0,1[
+	double prv = atof(argv[4]); // probabilidad de recuperacion mayores ]0,1[
+	int poi = atoi(argv[5]);    // cantidad de personas originalmente infectadas ]0,100]
+	double toc = atof(argv[6]); // tasa de ocupación ]0,1[ 
+	int dmn = atoi(argv[7]);     // duración minima de la enfermedad ]0,100]
+	int dmx = atoi(argv[8]);     // duración máxima de la enfermedad ]0,100]
+	int rmj = atoi(argv[9]);     // radio de movilidad de jovenes [0,5]
+	int rmm = atoi(argv[10]);    // radio de movilidad de mayores [0,5]
+	int vmj = atoi(argv[11]);    // velocidad de movimiento jovenes [0,5]
+	int vmm = atoi(argv[12]);    // velocidad de movimiento mayores [0,5]
+	int drc = atoi(argv[13]);    // duración de la siomulación ]0,100000]
+    int nhilos = atoi(argv[14]); // cantidad de hilos
     srand(time(NULL));
     ofstream resultado;
     resultado.open("resultado.csv");
     resultado << "Porcentaje de Susceptibles, Cantidad de Suceptibles, Porcentaje de Infectados, Cantidad de Infectados, Porcentaje de Recuperados, Cantidad de Recuperados, Porcentaje de Muertos, Cantidad de Muertos \n";
-    // TODO: PEDIRLO PARAM
-    int cpr = 4000000; // cantidad de personas int ]0,10000000]
-    double piv = 0.7; // potencia infecciosa ]0,1[
-    double prj = 0.75; // probabilidad de recuperacion jovenes ]0,1[
-    double prv = 0.5; // probabilidad de recuperacion mayores ]0,1[
-    int poi = 4000; // cantidad de personas originalmente infectadas ]0,100]
-    double toc = 0.3; // tasa de ocupación ]0,1[ 
-    int dmn = 50; // duración minima de la enfermedad ]0,100]
-    int dmx = 100; // duración máxima de la enfermedad ]0,100]
-    int rmj = 4; // radio de movilidad de jovenes [0,5]
-    int rmm = 2; // radio de movilidad de mayores [0,5]
-    int vmj = 3; // velocidad de movimiento jovenes [0,5]
-    int vmm = 2; // velocidad de movimiento mayores [0,5]
-    int drc = 365; // duración de la siomulación ]0,100000]
+    //// TODO: PEDIRLO PARAM
+    //int cpr = 4000000; // cantidad de personas int ]0,10000000]
+    //double piv = 0.7; // potencia infecciosa ]0,1[
+    //double prj = 0.75; // probabilidad de recuperacion jovenes ]0,1[
+    //double prv = 0.5; // probabilidad de recuperacion mayores ]0,1[
+    //int poi = 4000; // cantidad de personas originalmente infectadas ]0,100]
+    //double toc = 0.3; // tasa de ocupación ]0,1[ 
+    //int dmn = 50; // duración minima de la enfermedad ]0,100]
+    //int dmx = 100; // duración máxima de la enfermedad ]0,100]
+    //int rmj = 4; // radio de movilidad de jovenes [0,5]
+    //int rmm = 2; // radio de movilidad de mayores [0,5]
+    //int vmj = 3; // velocidad de movimiento jovenes [0,5]
+    //int vmm = 2; // velocidad de movimiento mayores [0,5]
+    //int drc = 365; // duración de la siomulación ]0,100000]
     int tmm = floor(sqrt(cpr / toc)) + 1; // tamaño de la matriz
-    int nhilos = 16; // cantidad de hilos
+    //int nhilos = 16; // cantidad de hilos
     vector<vector<vector<Persona> > > espacio(tmm, vector<vector<Persona>>(tmm, vector<Persona>()));
     vector<vector<vector<Persona> > > espacionuevo(tmm, vector<vector<Persona>>(tmm, vector<Persona>()));
     //vector<vector<int>> numInfectados(tmm, vector<int>(tmm));
@@ -254,4 +268,5 @@ int main()
     }
     final = omp_get_wtime();
     printf("tiempo final:%f", final - inicio);
+    resultado.close();
 }
